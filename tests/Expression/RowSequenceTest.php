f7393aedb2958a399ad5716a08a62b2eef57a836
Martí Adrogué <marti.adrogue@gmail.com>
2016-08-18 18:14:49 +0200

    Test case for gears of Builders

---
+++ tests/Expression/RowSequenceTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This file is part of the martiadrogue/sqlbuilder package.
+ *
+ * @author    Martí Adrogué <marti.adrogue@gmail.com>
+ * @copyright 2016 Martí Adrogué
+ * @license   https://opensource.org/licenses/MIT MIT License
+ */
+namespace MartiAdrogue\SqlBuilder\Test\Expression;
+
+use MartiAdrogue\SqlBuilder\Exception\InvalidSqlSyntaxException;
+use MartiAdrogue\SqlBuilder\Expression\RowSequence;
+
+class RowSequenceTest extends \PHPUnit_Framework_TestCase
+{
+    private $rowSequence;
+
+    public function setUp()
+    {
+        $this->rowSequence = new RowSequence(['row1', 'row2', 'row3', 'row4']);
+    }
+
+    /**
+     * @tests
+     */
+    public function shouldConvertAnArrayToAListOfElementsSeparatedByComma()
+    {
+        $rows = $this->rowSequence->getChainOfRows();
+        $this->assertEquals(
+            'row1, row2, row3, row4',
+            $rows,
+            'Array of strings changes to one single string ensemble by comma.'
+        );
+    }
+}
